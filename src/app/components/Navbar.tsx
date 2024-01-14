1dd7b59c6f025dd051da93c4ab0329bea4af5c5b
Yug34 <vertilax1337@gmail.com>
2024-01-15 04:22:45 +0530

    :lipstick: ui(Nav): update Navbar

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,70 +1,29 @@
 "use client"
-
 import * as React from "react"
-import Link from "next/link"
 
 import {cn} from "@/lib/utils"
 import {
     NavigationMenu,
-    NavigationMenuContent,
-    NavigationMenuItem,
     NavigationMenuLink,
-    NavigationMenuList,
-    NavigationMenuTrigger,
     navigationMenuTriggerStyle,
 } from "@/components/ui/navigation-menu"
 import {ThemeToggler} from "@/app/components/ThemeToggler";
-
-const components: { title: string; href: string; description: string }[] = [
-    {
-        title: "Alert Dialog",
-        href: "/docs/primitives/alert-dialog",
-        description:
-            "A modal dialog that interrupts the user with important content and expects a response.",
-    },
-    {
-        title: "Hover Card",
-        href: "/docs/primitives/hover-card",
-        description:
-            "For sighted users to preview content available behind a link.",
-    },
-    {
-        title: "Progress",
-        href: "/docs/primitives/progress",
-        description:
-            "Displays an indicator showing the completion progress of a task, typically displayed as a progress bar.",
-    },
-    {
-        title: "Scroll-area",
-        href: "/docs/primitives/scroll-area",
-        description: "Visually or semantically separates content.",
-    },
-    {
-        title: "Tabs",
-        href: "/docs/primitives/tabs",
-        description:
-            "A set of layered sections of content—known as tab panels—that are displayed one at a time.",
-    },
-    {
-        title: "Tooltip",
-        href: "/docs/primitives/tooltip",
-        description:
-            "A popup that displays information related to an element when the element receives keyboard focus or the mouse hovers over it.",
-    },
-]
+import {GitHubLogoIcon} from "@radix-ui/react-icons";
 
 export function Navbar() {
     return (
         <NavigationMenu className={"absolute top-0 left-0 max-w-full w-full border-b-2 bg-card text-card-foreground shadow-sm mb-10"}>
-            <div className={"flex max-w-full w-full justify-end items-end"}>
+            <div className={"flex px-4  max-w-full w-full justify-between items-center md:px-12"}>
                 <a
                     rel={"noreferrer"}
                     target={"_blank"}
                     href="https://github.com/Yug34/image-editor"
                     className={navigationMenuTriggerStyle()}
                 >
+                    <GitHubLogoIcon className={"mr-2"}/>
                     Source Code
                 </a>
+                <div className={"text-lg"}>Image Editor</div>
                 <ThemeToggler/>
             </div>
         </NavigationMenu>
