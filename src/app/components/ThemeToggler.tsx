518e8ca9eead1d251d58dbea7f63ccf47a6e29a1
Yug34 <vertilax1337@gmail.com>
2024-01-14 14:48:54 +0530

    :lipstick: ui(project): navbar

--- src/app/components/ThemeToggler.tsx
+++ src/app/components/ThemeToggler.tsx
@@ -17,7 +17,7 @@ export const ThemeToggler = () => {
 
     return (
         <DropdownMenu>
-            <DropdownMenuTrigger asChild>
+            <DropdownMenuTrigger className={"border-none"} asChild>
                 <Button variant="outline" size="icon">
                     <SunIcon className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
                     <MoonIcon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,14 @@
 "use client";
 import {FieldValues, useForm} from "react-hook-form";
 import {useEffect, useState} from "react";
-import { CardTitle, CardDescription, CardHeader, CardContent, Card } from "@/components/ui/card"
-import { Label } from "@/components/ui/label"
-import { Input } from "@/components/ui/input"
-import { Button } from "@/components/ui/button"
+import {CardTitle, CardDescription, CardHeader, CardContent, Card} from "@/components/ui/card"
+import {Label} from "@/components/ui/label"
+import {Input} from "@/components/ui/input"
+import {Button} from "@/components/ui/button"
 import {redirect} from "next/navigation";
 import Editor from "@/app/components/Editor";
 import {ThemeToggler} from "@/app/components/ThemeToggler";
+import {Navbar} from "@/app/components/Navbar";
 
 export default function Home() {
     const {register, handleSubmit} = useForm();
@@ -20,43 +21,40 @@ export default function Home() {
 
     return (
         <>
-            <div className={"w-full h-20 flex justify-end items-center"}>
-                <ThemeToggler/>
-            </div>
-            {
-                isLoggedIn ? (
-                    <Editor/>
-                ) : (
-                    <Card className="mx-auto max-w-sm">
-                        <CardHeader className="space-y-1">
-                            <CardTitle className="text-2xl font-bold">Login</CardTitle>
-                            <CardDescription>Enter your email and password to login to your account</CardDescription>
-                        </CardHeader>
-                        <CardContent>
-                            {/*<form onSubmit={handleSubmit((data) => {*/}
-                            {/*    setData(data);*/}
-                            {/*    setIsLoggedIn(true);*/}
-                            {/*})}>*/}
-                            {/*    <div className="space-y-4">*/}
-                            {/*        <div className="space-y-2">*/}
-                            {/*            <Label htmlFor="email">Email</Label>*/}
-                            {/*            <Input {...register("email")} id="email" placeholder="user@example.com" required type="email" />*/}
-                            {/*        </div>*/}
-                            {/*        <div className="space-y-2">*/}
-                            {/*            <Label htmlFor="password">Password</Label>*/}
-                            {/*            <Input {...register("password")} id="password" required type="password" />*/}
-                            {/*        </div>*/}
-                            {/*        <Button className="w-full" type="submit">*/}
-                            {/*            Login*/}
-                            {/*        </Button>*/}
-                            {/*    </div>*/}
-                            {/*</form>*/}
-                            <button onClick={() => {
-                                setIsLoggedIn(true);
-                            }}>click</button>
-                        </CardContent>
-                    </Card>
-                )
+            <Navbar/>
+            {isLoggedIn ?
+                <Editor/>
+                :
+                <Card className="mx-auto max-w-sm">
+                    <CardHeader className="space-y-1">
+                        <CardTitle className="text-2xl font-bold">Login</CardTitle>
+                        <CardDescription>Enter your email and password to login to your account</CardDescription>
+                    </CardHeader>
+                    <CardContent>
+                        {/*<form onSubmit={handleSubmit((data) => {*/}
+                        {/*    setData(data);*/}
+                        {/*    setIsLoggedIn(true);*/}
+                        {/*})}>*/}
+                        {/*    <div className="space-y-4">*/}
+                        {/*        <div className="space-y-2">*/}
+                        {/*            <Label htmlFor="email">Email</Label>*/}
+                        {/*            <Input {...register("email")} id="email" placeholder="user@example.com" required type="email" />*/}
+                        {/*        </div>*/}
+                        {/*        <div className="space-y-2">*/}
+                        {/*            <Label htmlFor="password">Password</Label>*/}
+                        {/*            <Input {...register("password")} id="password" required type="password" />*/}
+                        {/*        </div>*/}
+                        {/*        <Button className="w-full" type="submit">*/}
+                        {/*            Login*/}
+                        {/*        </Button>*/}
+                        {/*    </div>*/}
+                        {/*</form>*/}
+                        <button onClick={() => {
+                            setIsLoggedIn(true);
+                        }}>click
+                        </button>
+                    </CardContent>
+                </Card>
             }
         </>
     )
